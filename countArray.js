5d8840326717ead8d4355fa5bf83f31500637073
Emma Craig <emma.ryal@gmail.com>
2021-02-22 22:16:49 +0100

    regex - fully working

--- countArray.js
+++ countArray.js
@@ -1,23 +1,11 @@
-function countChar(string) {
-  const splitStr = string.split("");
-  let a = b = c = 0;
+const countChar = (string) => {
+  let arr = (count = []);
 
-  let count_a = ["a", a];
-  let count_b = ["b", b];
-  let count_c = ["c", c];
-  for (i = 0; i < string.length; i++)
-    switch (splitStr[i]) {
-      case "a":
-        count_a[1]++;
-        break;
-      case "b":
-        count_b[1]++;
-        break;
-      case "c":
-        count_c[1]++;
-        break;
-      default:
-        break;
-    }
-  return (count = [count_a, count_b, count_c]);
-}
+  string.replace(/(.)\1*/g, function (str, char) {
+    count = [char, str.length];
+
+    arr.push(count);
+  });
+
+  return arr;
+};
