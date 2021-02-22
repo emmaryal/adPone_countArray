6a6e93a108cebebb34f2da7c80c4d421b883863e
Emma Craig <emma.ryal@gmail.com>
2021-02-22 19:10:58 +0100

    first commit

---
+++ countArray.js
@@ -0,0 +1,20 @@
+function countChar(string) {
+  let count = [];
+  let splitStr = string.split("");
+  let a = (b = c = 0);
+  let count_a = ["a", a];
+  for (i = 0; i < string.length; i++) if (splitStr[i] === "a") count_a[1]++;
+
+  let b = 0;
+  let count_b = ["b", b];
+
+  for (i = 0; i < string.length; i++) if (splitStr[i] === "b") count_b[1]++;
+
+  let c = 0;
+  let count_c = ["c", c];
+
+  for (i = 0; i < string.length; i++) if (splitStr[i] === "c") count_c[1]++;
+  count_c;
+
+  return (count = [count_a, count_b, count_c]);
+}
